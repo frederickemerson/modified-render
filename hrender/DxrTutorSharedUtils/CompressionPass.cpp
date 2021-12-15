185671e6221039eb64fad5894bc8f64c22332d84
nicholasnge <59093518+nicholasnge@users.noreply.github.com>
2021-12-16 06:40:58 +0800

    test bug fix

--- hrender/DxrTutorSharedUtils/CompressionPass.cpp
+++ hrender/DxrTutorSharedUtils/CompressionPass.cpp
@@ -18,23 +18,26 @@ void CompressionPass::execute(RenderContext* pRenderContext)
     if (mMode == Mode::Compression) {
         // Loop over all textures, compress each one
         for (int i = 0; i < RenderConfig::mConfig.size(); i++) {
+            {
+                std::lock_guard lock(NetworkManager::mMutexServerVisTexRead);
 
-            // Parameters for Compression
-            const char* const sourceBuffer = reinterpret_cast<const char* const>(RenderConfig::mConfig[i].cpuLocation);
-            int sourceBufferSize = RenderConfig::BufferTypeToSize(RenderConfig::mConfig[i].type);
+                // Parameters for Compression
+                const char* const sourceBuffer = reinterpret_cast<const char* const>(RenderConfig::mConfig[i].cpuLocation);
+                int sourceBufferSize = RenderConfig::BufferTypeToSize(RenderConfig::mConfig[i].type);
 
-            // Compress buffer
-            int compressedSize = LZ4_compress_default(sourceBuffer, outputBuffer, sourceBufferSize, sourceBufferSize);
+                // Compress buffer
+                int compressedSize = LZ4_compress_default(sourceBuffer, outputBuffer, sourceBufferSize, sourceBufferSize);
 
-            // Update size of compressed buffer
-            RenderConfig::mConfig[i].compressedSize = compressedSize;
+                // Update size of compressed buffer
+                RenderConfig::mConfig[i].compressedSize = compressedSize;
 
-            // Update location of buffer
-            RenderConfig::mConfig[i].cpuLocation = outputBuffer;
+                // Update location of buffer
+                RenderConfig::mConfig[i].cpuLocation = outputBuffer;
 
-            char buffer[140];
-            sprintf(buffer, "\n\n= Compressed Buffer: Original size: %d, Compressed size: %d =========", sourceBufferSize, compressedSize);
-            OutputDebugStringA(buffer);
+                char buffer[140];
+                sprintf(buffer, "\n\n= Compressed Buffer: Original size: %d, Compressed size: %d =========", sourceBufferSize, compressedSize);
+                OutputDebugStringA(buffer);
+            }
         }
     }
 
@@ -43,20 +46,22 @@ void CompressionPass::execute(RenderContext* pRenderContext)
     else { // mMode == Mode::Decompression
         // Loop over all buffers, decompress each one
         for (int i = 0; i < RenderConfig::mConfig.size(); i++) {
+            {
+                std::lock_guard lock(NetworkManager::mMutexClientVisTexRead);
+                // Parameters for Decompression
+                const char* const sourceBuffer = reinterpret_cast<const char* const>(RenderConfig::mConfig[i].cpuLocation);
+                int sourceBufferSize = RenderConfig::mConfig[i].compressedSize;
+                int maxDecompressedSize = RenderConfig::BufferTypeToSize(RenderConfig::mConfig[i].type);
 
-            // Parameters for Decompression
-            const char* const sourceBuffer = reinterpret_cast<const char* const>(RenderConfig::mConfig[i].cpuLocation);
-            int sourceBufferSize = RenderConfig::mConfig[i].compressedSize;
-            int maxDecompressedSize = RenderConfig::BufferTypeToSize(RenderConfig::mConfig[i].type);
+                // Decompress buffer
+                int decompressedSize = LZ4_decompress_safe(sourceBuffer, outputBuffer, sourceBufferSize, maxDecompressedSize);
 
-            // Decompress buffer
-            int decompressedSize = LZ4_decompress_safe(sourceBuffer, outputBuffer, sourceBufferSize, maxDecompressedSize);
+                // Update size of decompressed buffer
+                RenderConfig::mConfig[i].compressedSize = decompressedSize;
 
-            // Update size of decompressed buffer
-            RenderConfig::mConfig[i].compressedSize = decompressedSize;
-
-            // Update location of buffer
-            RenderConfig::mConfig[i].cpuLocation = outputBuffer;
+                // Update location of buffer
+                RenderConfig::mConfig[i].cpuLocation = outputBuffer;
+            }
         }
     }
 }
